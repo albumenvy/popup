7afd855f451bceac3abac5ce7fee8ab6e61e7217
Dmitry Fedyuk <dfediuk@gmail.com>
2018-05-21 03:05:26 +0600

    0.0.2

--- view/frontend/web/customize.js
+++ view/frontend/web/customize.js
@@ -1,9 +1,20 @@
 // 2018-05-19
-define(['jquery', 'mage/utils/wrapper'], function($, w) {'use strict'; return function(sb) {
+define([
+	'jquery', 'mage/utils/wrapper', 'Magento_Ui/js/modal/modal'
+], function($, w, modal) {'use strict'; return function(sb) {
 $.extend(sb.prototype, {
 	submit: w.wrap(sb.prototype.submit, function(_super) {
 		debugger;
-		return _super();
+		// 2018-05-21
+		// How is the `mage.modal` jQuery UI widget implemented and used?
+		// https://mage2.pro/t/5518
+		var m = $('<div>TEST</div>').modal({
+			buttons: [],
+			modalClass: 'albumenvy-popup',
+			type: 'popup'
+		});
+		m.modal('openModal');
+		//return _super();
 	})
 });
 return sb; };});
